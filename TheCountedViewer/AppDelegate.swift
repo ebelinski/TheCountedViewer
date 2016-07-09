5b84a4e9777172b889d9547d8525f08b0c38b5ba
Eugeniu Belinski <eugeniub@gmail.com>
2016-07-09 17:23:53 -0500

    added global Service instance

--- TheCountedViewer/AppDelegate.swift
+++ TheCountedViewer/AppDelegate.swift
@@ -1,9 +1,12 @@
 import UIKit
+import Siesta
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+
+  let API = Service(baseURL: "https://thecountedapi.com/api/counted")
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     return true
