3c79aa7e3b231ae6a39847898bf8379d91611ab0
Eugeniu Belinski <eugeniub@gmail.com>
2016-07-09 17:28:59 -0500

    added API observer for MapViewController

--- TheCountedViewer/MapViewController.swift
+++ TheCountedViewer/MapViewController.swift
@@ -1,12 +1,19 @@
 import UIKit
 import MapKit
+import Siesta
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, ResourceObserver {
 
   @IBOutlet weak var mapView: MKMapView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    API.resource("/").addObserver(self)
+  }
+
+  func resourceChanged(resource: Resource, event: ResourceEvent) {
+    print("resource changed")
   }
 
 }
