35c02027b965ee9fd7f552f69579dee0f72789dd
Eugeniu Belinski <eugeniub@gmail.com>
2016-07-09 17:43:15 -0500

    removed Pods directory from tracking

--- Pods/Target Support Files/Pods-TheCountedViewer/Pods-TheCountedViewer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TheCountedViewer : NSObject
-@end
-@implementation PodsDummy_Pods_TheCountedViewer
-@end
--- Pods/Target Support Files/Siesta/Siesta-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "Siesta.h"
-
-FOUNDATION_EXPORT double SiestaVersionNumber;
-FOUNDATION_EXPORT const unsigned char SiestaVersionString[];
-
