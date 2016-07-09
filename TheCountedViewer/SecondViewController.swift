ecf54045769b787aa0a058757e1575843cf061c9
Eugeniu Belinski <eugeniub@gmail.com>
2016-07-09 12:52:07 -0500

    removed junk

--- TheCountedViewer/SecondViewController.swift
+++ TheCountedViewer/SecondViewController.swift
@@ -1,25 +1,10 @@
-//
-//  SecondViewController.swift
-//  TheCountedViewer
-//
-//  Created by Eugeniu Belinski on 16/7/8.
-//  Copyright © 2016 Eugeniu Belinski. All rights reserved.
-//
-
 import UIKit
 
 class SecondViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
