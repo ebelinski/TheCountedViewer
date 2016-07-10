538b4a837c0e20edbad8b3ca09e982086b1dbfd1
Eugeniu Belinski <eugeniub@gmail.com>
2016-07-10 09:33:10 -0500

    created siesta transformer for "/counted"

--- TheCountedViewer/AppDelegate.swift
+++ TheCountedViewer/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Siesta
+import SwiftyJSON
 
 let API = Service(baseURL: "https://thecountedapi.com/api")
 
@@ -9,6 +10,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+    API.configureTransformer("/counted") {
+      ($0.content as JSON).arrayValue.map(Killing.init)
+    }
+
     return true
   }
 }
