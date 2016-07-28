59b9228104b4a2a55c3bfa452e0e08cda7325fca
Eugeniu Belinski <eugeniub@gmail.com>
2016-07-28 11:55:35 -0500

    created instance const resource

--- TheCountedViewer/MapViewController.swift
+++ TheCountedViewer/MapViewController.swift
@@ -6,12 +6,19 @@ class MapViewController: UIViewController, ResourceObserver {
 
   @IBOutlet weak var mapView: MKMapView!
 
+  let resource = API.resource("/counted")
   var incidents: [Incident] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    API.resource("/counted").addObserver(self).loadIfNeeded()
+    resource.addObserver(self)
+  }
+
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+
+    resource.loadIfNeeded()
   }
 
   func resourceChanged(resource: Resource, event: ResourceEvent) {
