6561893e0a22ae55182b1a4dd8f22ea911ff5419
Eugeniu Belinski <eugeniub@gmail.com>
2016-08-01 22:50:07 -0500

    added age to Incident

--- TheCountedViewer/Incident.swift
+++ TheCountedViewer/Incident.swift
@@ -3,8 +3,14 @@ import SwiftyJSON
 
 struct Incident {
   let name: String?
+  let age: Int?
 
   init(json: JSON) {
     name = json["name"].string
+    if let ageString = json["age"].string {
+      age = Int(ageString)
+    } else {
+      age = nil
+    }
   }
 }
