821e633c1e237af277d044c490c7ff06b68bc99d
Eugeniu Belinski <eugeniub@gmail.com>
2016-08-01 22:10:59 -0500

    added API configuration for JSON content type

--- TheCountedViewer/AppDelegate.swift
+++ TheCountedViewer/AppDelegate.swift
@@ -10,6 +10,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    API.configure {
+      $0.config.responseTransformers.add(ResponseContentTransformer(skipWhenEntityMatchesOutputType: false) { JSON($0.content as AnyObject) }, contentTypes: ["*/json"])
+    }
 
     API.configureTransformer("/counted") {
       ($0.content as JSON).arrayValue.map(Incident.init)
