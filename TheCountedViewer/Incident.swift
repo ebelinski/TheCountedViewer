c2b0b73411c6146ca64f1d4c5ff11bcad5aada39
Eugeniu Belinski <eugeniub@gmail.com>
2016-08-01 23:16:04 -0500

    added ethnicity to Incident

--- TheCountedViewer/Incident.swift
+++ TheCountedViewer/Incident.swift
@@ -4,13 +4,29 @@ import SwiftyJSON
 struct Incident {
   let name: String?
   let age: Int?
+  let ethnicity: Ethnicity
 
   init(json: JSON) {
     name = json["name"].string
+
     if let ageString = json["age"].string {
       age = Int(ageString)
     } else {
       age = nil
+    }
+
+    if let raceString = json["race"].string {
+      switch raceString {
+      case "White": ethnicity = .White
+      case "Black": ethnicity = .Black
+      case "Hispanic/Latino": ethnicity = .HispanicOrLatino
+      case "Asian/Pacific Islander": ethnicity = .AsianOrPacificIslander
+      case "Native American": ethnicity = .NativeAmerican
+      case "Other": ethnicity = .Other
+      default: ethnicity = .Unknown
+      }
+    } else {
+      ethnicity = .Unknown
     }
   }
 }
