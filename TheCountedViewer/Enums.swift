58a21cffa226708935129f9eb65d4ba549da1785
Eugeniu Belinski <eugeniub@gmail.com>
2016-08-01 22:52:15 -0500

    created Ethnicity and Gender enums

---
+++ TheCountedViewer/Enums.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+enum Ethnicity {
+  case White
+  case Black
+  case HispanicOrLatino
+  case AsianOrPacificIslander
+  case NativeAmerican
+  case Other
+  case Unknown
+}
+
+enum Gender {
+  case Female
+  case Male
+  case NonConforming
+  case Unknown
+}
